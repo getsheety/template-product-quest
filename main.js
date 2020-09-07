ed39bc68dfd37f8de18b450e3784f9a0d441bf9b
Phillip Caudell <phill@caudell.co.uk>
2020-09-07 18:09:15 +0100

    Updates the URL to avoid being sued :-)

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', function(event) {
 
 // Replace this with your Sheety URL
 // Make sure NOT to include the sheet name in the URL (just the project name!)
-var projectUrl = 'https://api.sheety.co/phill/productHunt';
+var projectUrl = 'https://api.sheety.co/phill/productQuest';
 
 function loadProducts() {
 	fetch(projectUrl + '/products')
