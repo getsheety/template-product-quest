d80ae959ca1f722168decc5a4aeca1000b4c5910
Phillip Caudell <phill@caudell.co.uk>
2020-09-07 18:03:57 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Product Quest Template
+Take a look at how to create a 'Product Hunt' like directory with upvoting functionality.
+
+## How to use
+1. Open main.js in a text editor and locate the Sheety URL
+2. Replace the project URL with your own Sheety URL (make sure *not* to include the sheet name in the URL)
+3. Enable 'POST' requests on the "Products" sheet in the Sheety Dashboard
+4. Customise as you please!
+5. Voila! A directory with upvoting powered by a spreadsheet
+
+## License
+Copyright 2020 Phillip Caudell
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ main.js
@@ -0,0 +1,78 @@
+var products = null;
+var categories = null;
+var title = null;
+
+// Called once the page has loaded
+document.addEventListener('DOMContentLoaded', function(event) {
+	loadProducts();
+	loadCategories();
+});
+
+// Replace this with your Sheety URL
+// Make sure NOT to include the sheet name in the URL (just the project name!)
+var projectUrl = 'https://api.sheety.co/phill/productHunt';
+
+function loadProducts() {
+	fetch(projectUrl + '/products')
+	.then((response) => response.json())
+	.then(json => {
+		this.products = json.products.sort((a, b) => {
+			return a.votes < b.votes;
+		})
+		showAllProducts();
+	});
+}
+
+function loadCategories() {
+	fetch(projectUrl + '/categories')
+	.then((response) => response.json())
+	.then(json => {
+		this.categories = json.categories;
+		drawCategories();
+	})
+}
+
+function drawProducts(products) {
+	var template = Handlebars.compile(document.getElementById("products-template").innerHTML);
+	document.getElementById('products-container').innerHTML = template({
+		title: this.title,
+		products: products	
+	});
+}
+
+function drawCategories() {
+	var template = Handlebars.compile(document.getElementById("menu-template").innerHTML);
+	console.log('draw ', this.products);
+	document.getElementById('menu-container').innerHTML = template(this.categories);
+}
+
+function showAllProducts() {
+	this.title = "All Products";
+	drawProducts(this.products);
+}
+
+function showCategory(category) {
+	this.title = category;
+	let filteredProducts = this.products.filter(product => {
+		return product.category == category;
+	});
+	drawProducts(filteredProducts);
+}
+
+function upvoteProduct(id) {
+	let product = this.products.find(product => {
+		return product.id == id;
+	});
+	product.votes = product.votes + 1;
+	product.hasVoted = true;
+	
+	let headers = new Headers();
+	headers.set('content-type', 'application/json');
+	fetch(projectUrl + '/products/' + id, {
+		method: 'PUT',
+		body: JSON.stringify({ product: product }),
+		headers: headers
+	});
+	
+	showAllProducts();
+}
